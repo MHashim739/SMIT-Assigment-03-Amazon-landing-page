1f2033d2a04431840123b3838c4e2b84b32f126a
Muhammad hashim <mh4165209@gmail.com>
2026-05-19 23:52:49 -0700

    navbar 01 dropdown complete

---
+++ app.js
@@ -0,0 +1,7 @@
+// nav 01 signin button event
+function showCard(){
+    document.getElementById("card").style.display="block";
+}
+function hideCard(){
+    document.getElementById("card").style.display="none"
+}
