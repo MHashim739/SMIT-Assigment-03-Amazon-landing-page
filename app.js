675ba0bdc1958284da40f6bdc81480fe029522ad
Muhammad hashim <mh4165209@gmail.com>
2026-05-20 21:32:29 -0700

    nav banner slider complete  not responsive

--- app.js
+++ app.js
@@ -32,3 +32,26 @@ function closeMenu(){
 //     sidebar.classList.toggle("active");
 
 // }
+
+// section 3 slider js
+
+let index = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(i){
+    if(i >= slides.length) index = 0;
+    else if(i < 0) index = slides.length - 1;
+    else index = i;
+
+    document.querySelector(".slides").style.transform =
+        `translateX(${-index * 100}%)`;
+}
+
+function changeSlide(step){
+    showSlide(index + step);
+}
+
+// auto slide every 6 seconds
+setInterval(() => {
+    changeSlide(1);
+}, 6000);
