9dc97807bdae341c729dca6d9446c66f6b3634b0
Muhammad hashim <mh4165209@gmail.com>
2026-05-20 18:51:39 -0700

    nav bar 02 half complete

--- app.js
+++ app.js
@@ -5,3 +5,30 @@ function showCard(){
 function hideCard(){
     document.getElementById("card").style.display="none"
 }
+
+// hamburger JS
+
+function openMenu(){
+
+    var sidebar = document.getElementById("sidebar");
+
+    sidebar.classList.add("active");
+
+}
+
+function closeMenu(){
+
+    var sidebar = document.getElementById("sidebar");
+
+    sidebar.classList.remove("active");
+
+}
+
+
+// function openMenu(){
+
+//     var sidebar = document.getElementById("sidebar");
+
+//     sidebar.classList.toggle("active");
+
+// }
